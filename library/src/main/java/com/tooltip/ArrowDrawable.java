991a578e764e31afefec8196e10d27a0ed83775b
Viнt@rь <vihtarb@gmail.com>
2016-05-31 16:17:06 +0300

    Initial commit

---
+++ library/src/main/java/com/tooltip/ArrowDrawable.java
@@ -0,0 +1,10 @@
+package com.tooltip;
+
+import android.graphics.drawable.ColorDrawable;
+
+/**
+ * Created by Viнt@rь on 26.05.2016
+ */
+class ArrowDrawable extends ColorDrawable {
+
+}
---
+++ library/src/main/java/com/tooltip/Tooltip.java
@@ -0,0 +1,14 @@
+package com.tooltip;
+
+/**
+ * Created by Viнt@rь on 26.05.2016
+ */
+public class Tooltip {
+
+    private Tooltip() {
+    }
+
+    public static final class Builder {
+
+    }
+}

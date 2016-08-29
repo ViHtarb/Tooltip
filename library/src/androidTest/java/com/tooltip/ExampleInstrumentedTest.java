8316e105bb85fc36ed1230eac37e7e0572a9a5d3
ViHtarb <vihtarb@gmail.com>
2016-08-29 15:05:43 +0300

    Fixed library example instrumented test

--- library/src/androidTest/java/com/tooltip/ExampleInstrumentedTest.java
+++ library/src/androidTest/java/com/tooltip/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.tooltip", appContext.getPackageName());
+        assertEquals("com.tooltip.test", appContext.getPackageName());
     }
 }
